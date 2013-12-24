7fff76cb222711c267dbff1d8a1fe172c3107a08
ghanshyam <ghanshyam@linuxmint.linuxmint-domain>
2013-12-24 23:32:09 +0530

    hello i have set up enviro ment on diffren machine i.e laptop

--- hellodjango/wsgi.py
+++ hellodjango/wsgi.py
@@ -6,18 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
+'''
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
-'''
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-'''
+
 '''
 To run on local machine just comment below line and un comment above two lines
-'''
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
-
