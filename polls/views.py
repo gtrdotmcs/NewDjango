0fb922d6d5eb46f73c44ae3a322db2e0a6d1881a
gtrdotmcs <gtr.mcs@gmail.com>
2013-12-24 19:06:19 +0530

    admin url link updated

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    conrlink = """<br/><a href="http://www.freedomain.co.nr/" target="_blank" title="Free Domain Forwarding"><img src="http://umuea.imdrv.net/623/2.gif" width="88" height="31" border="0" alt="Free Domain Forwarding" /></a>"""
+    conrlink = """<br/><a href="http://www.freedomain.co.nr/" target="_blank" title="Free Domain Forwarding"><img src="http://umuea.imdrv.net/623/2.gif" width="88" height="31" border="0" alt="Free Domain Forwarding" /></a>
+<br/><a href="http://newdjangogtr.herokuapp.com/admin/"> admin url </a>"""
     return HttpResponse("Hello, world. You're at the polls index."+conrlink)
 
 def detail(request, poll_id):
