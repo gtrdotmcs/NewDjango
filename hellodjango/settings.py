edd3715ecdc45ff2b791db0d61bcfc3a0160ccaa
gtrdotmcs <gtr.mcs@gmail.com>
2013-12-24 17:29:23 +0530

    pools is now added to the admin section dude

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -55,7 +55,7 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
@@ -79,7 +79,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from polls.models import Choice, Poll
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Poll, PollAdmin)
+
