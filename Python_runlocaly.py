0b52f1e2557e90838ddab64469806f79ea8784bf
kshaikh <sthorat@sthorat-ThinkCentre-M55e.(none)>
2014-01-02 18:12:28 +0530

    localurunning the code ans changes to the heroku and adding new templet folder

--- Python_runlocaly.py
+++ Python_runlocaly.py
@@ -1,36 +1,42 @@
 import sys
 from subprocess import Popen, PIPE
 
-global line
 
 def changegitConfigfile():
-    global line
-    file_read = open("setting.py","r")
+    line = ""
+    file_reads = open("hellodjango/settings.py","r")
     #previousline = ""
-    line = file_read.read()
-    file_read.close()
-    line2 = line.replace("TalentBeat@bitbucket", "TalentBeat:Talent_123!@bitbucket")
-    #file_write = open(".git/config","w")
-    #file_write.write(line2)
-    #file_write.close()
+    line = file_reads.read()
+    file_reads.close()
+    if line.find("''' RunLocaly Comment and Uncomment Run Heroku(Do not change this comment line)") != -1:
+        line2 = line.replace("''' RunLocaly Comment and ", "#''' RunLocaly Comment and ")
+    if line2.find("''' RunLocaly Comment and Uncomment Run Heroku(Do not change this comment line)") != -1:
+        line2 = line2.replace("#''' Setting RunLocaly unComment and comment Run Heroku","''' Setting RunLocaly unComment and comment Run Heroku")    
+    file_write = open("hellodjango/settings.py","w")
+    file_write.write(line2)
+    file_write.close()
+    line = ""
+    line2 = ""
+    file_readw = open("hellodjango/wsgi.py","r")
+    line = file_readw.read()
+    file_readw.close()
+    if line.find("'''RunLocaly Comment and  Uncomment Run Heroku(Do not change this comment line)") != -1:
+        line2 = line.replace("'''RunLocaly Comment and  ", "#'''RunLocaly Comment and  ")
+    file_write = open("hellodjango/settings.py","w")
+    file_write.write(line2)
+    file_write.close()    
 
-def revertChnagetoconfigfile():    
-    global line
-    print line
-    #file_write = open(".git/config","w")
-    #file_write.write(line)
-    #file_write.close()
-    
+
        
 if __name__ == "__main__":
-    
+    changegitConfigfile()
     if len(sys.argv) == 3:
         changegitConfigfile() 
-        proc = Popen(['bash',sys.argv[1]], stdin=PIPE)
-        proc.communicate('Y')
-        revertChnagetoconfigfile()
-        proc = Popen(['bash',sys.argv[2]], stdin=PIPE)
-        proc.communicate('Y')
+        #proc = Popen(['bash',sys.argv[1]], stdin=PIPE)
+        #proc.communicate('Y')
+        #revertChnagetoconfigfile()
+        #proc = Popen(['bash',sys.argv[2]], stdin=PIPE)
+        #proc.communicate('Y')
     else:
         print "This need to be in Clone dir and Bash scripts are not provided"
         print "python Changedotgitconfig.py \"gitpull script\" \"run foreman script\" "     
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,34 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
-
+#from django.template import RequestContext, loader
+'''
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
+'''
+#from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from polls.models import Poll
 
 def index(request):
-    conrlink = "<br/><a href='http://newdjangogtr.herokuapp.com/admin/'>admin url </a>"
-    return HttpResponse("Hello, world. You're at the polls index."+conrlink)
+    
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    '''
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    '''
+    #return HttpResponse("You're looking at poll %s." % poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
